de6bb09271e1fa95bc530be8d89455495af53e72
iantshaw <80100023+iantshaw@users.noreply.github.com>
2022-10-13 12:58:46 +0100

    Another reference to layout removed

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -4,16 +4,6 @@ import Link from 'next/link';
 
 export default function FirstPost() {
   return (
-    <Layout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </Layout>
+    
   );
 }
